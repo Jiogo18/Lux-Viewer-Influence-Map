622cc2279dac3ea222196831dcdcb40270bc808b
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2024-10-25 21:57:23 -0400

    Influence map without buffer

--- src/scenes/InfluenceMap.ts
+++ src/scenes/InfluenceMap.ts
@@ -39,7 +39,6 @@ export const INFLUENCE_MAP_PROPS_DEFAULT: InfluenceMapProps = {
 
 export class InfluenceMap {
   private dataTiles: Map<number, number> = new Map();
-  private bufferDataTiles: Map<number, number> = new Map();
   private cooldown: number = 0;
 
   /**
@@ -54,7 +53,6 @@ export class InfluenceMap {
   init(tilesPositionHash: number[]) {
     for (let positionHash of tilesPositionHash) {
       this.dataTiles.set(positionHash, 0);
-      this.bufferDataTiles.set(positionHash, 0);
     }
   }
 
@@ -72,33 +70,31 @@ export class InfluenceMap {
     if (this.cooldown > 0) return;
     this.cooldown = this.props.updateCooldown;
 
-    // Fill the buffer and set the base influence
-    for (let [p, prevValue] of this.dataTiles) {
-      const baseValue = this.getBaseInfluence(p, frame);
-
-      if (Math.abs(prevValue) < Math.abs(baseValue)) {
-        this.bufferDataTiles.set(p, baseValue);
-      } else {
-        this.bufferDataTiles.set(p, prevValue);
-      }
-    }
+    // Fill the buffer
+    const bufferDataTiles = new Map(this.dataTiles);
 
     // Propagate and store in the final map
-    for (let [p, prevValue] of this.bufferDataTiles) {
+    for (let [p, prevValue] of bufferDataTiles) {
+      const baseValue = this.getBaseInfluence(p, frame);
+
       const valueNeighbours = this.getNeighbours(p)
-        .map((n) => this.bufferDataTiles.get(n))
+        .map((n) => bufferDataTiles.get(n))
         .filter((v) => v !== undefined)
         .map((v) => this.applyDecay(v));
       const maxInf = valueNeighbours.reduce((p, c) => Math.max(p, c), 0);
       const minInf = valueNeighbours.reduce((p, c) => Math.min(p, c), 0);
 
       const mostDominant = -minInf > maxInf ? minInf : maxInf;
-      const newValue = linearInterpolation(
-        prevValue,
-        mostDominant,
-        this.props.momentum
-      );
-      this.dataTiles.set(p, newValue);
+      if (Math.abs(mostDominant) < Math.abs(baseValue)) {
+        this.dataTiles.set(p, baseValue);
+      } else {
+        const newValue = linearInterpolation(
+          prevValue,
+          mostDominant,
+          this.props.momentum
+        );
+        this.dataTiles.set(p, newValue);
+      }
     }
   }
 
